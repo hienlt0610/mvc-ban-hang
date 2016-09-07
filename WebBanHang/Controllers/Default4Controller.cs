7272010381a42130dcf27450160be193a073b8f8
Tran Thanh Thanh <Thanhttstudio@gmail.com>
2016-09-07 12:17:34 +0700

    them cotroller lan 4

---
+++ WebBanHang/Controllers/Default4Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang.Controllers
+{
+    public class Default4Controller : Controller
+    {
+        //
+        // GET: /Default4/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
