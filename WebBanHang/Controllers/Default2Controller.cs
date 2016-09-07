1857e071d980ea8ec3ae537ca6ce6ce82ffbbd2e
Tran Thanh Thanh <Thanhttstudio@gmail.com>
2016-09-07 12:08:46 +0700

    them controllers lan 2

---
+++ WebBanHang/Controllers/Default2Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebBanHang.Controllers
+{
+    public class Default2Controller : Controller
+    {
+        //
+        // GET: /Default2/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
