087521f7531aaaf82949bdc2b2e177231cd8a735
Hien Le <hienlt0610@gmail.com>
2016-09-08 01:46:16 +0700

    Dung master layout

---
+++ WebBanHang/Utils/AssetUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebBanHang.Utils
+{
+    public class AssetUtil
+    {
+        public static String ResourceUrl(String path)
+        {
+            UrlHelper url = new UrlHelper(HttpContext.Current.Request.RequestContext);
+            return url.Content("~/Content/" + path);
+        }
+    }
+}
