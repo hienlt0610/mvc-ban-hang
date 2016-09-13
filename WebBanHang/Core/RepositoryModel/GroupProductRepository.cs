16ae24e48292d10aa4150f21790c9c498021a9e6
Hien Le <hienlt0610@gmail.com>
2016-09-14 03:04:54 +0700

    Add file

---
+++ WebBanHang/Core/RepositoryModel/GroupProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebBanHang.Models;
+
+namespace WebBanHang.Core.RepositoryModel
+{
+    public class GroupProductRepository : RepositoryModel<GroupProduct>
+    {
+        public GroupProductRepository(DbContext db) : base(db)
+        {
+            
+        }
+    }
+}
