edb9beef64fbe51525a0acd23fec7abaa3573410
Hien Le <hienlt0610@gmail.com>
2016-09-26 23:32:06 +0700

    add product detail and fix some bug

--- WebBanHang/App_Start/RouteConfig.cs
+++ WebBanHang/App_Start/RouteConfig.cs
@@ -12,11 +12,11 @@ namespace WebBanHang
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "WebBanHang.Controllers" }
             );
         }
     }
---
+++ WebBanHang/ViewModels/ProductViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebBanHang.ViewModels
+{
+    public class ProductViewModel
+    {
+        [Required]
+        public String ProductName { get; set; }
+
+        [Required]
+        public String Detail { get; set; }
+        
+        public int GroupID { get; set; }
+
+        [Required]
+        public long Price { get; set; }
+
+        [Required]
+        public long SalePrice { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+        public bool Active { get; set; }
+    }
+}
