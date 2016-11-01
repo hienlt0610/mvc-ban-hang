e7b6babbc18f3067471d38e2ee3bf299fbe7568d
Hien Le <hienlt0610@gmail.com>
2016-11-01 11:41:05 +0700

    optimize home admin

--- WebBanHang/Areas/Admin/Controllers/HomeController.cs
+++ WebBanHang/Areas/Admin/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace WebBanHang.Areas.Admin.Controllers
         [Security]
         public ActionResult Index()
         {
-            ViewBag.Orders = Repository.Order.FetchAll();
+            ViewBag.Orders = Repository.Order.FetchAll().OrderByDescending(o=>o.OrderDate).Take(10);
             ViewBag.Products = Repository.Product.FetchAll();
             ViewBag.Customers = Repository.Customer.FetchAll();
             return View("Index");
